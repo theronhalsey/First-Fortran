33bbc907adfe26187fa8a0565ffeb14fca7a2028
Theron Halsey <theron.halsey@gmail.com>
2022-01-06 18:28:45 -0500

    matrix

--- main.f90
+++ main.f90
@@ -8,6 +8,12 @@ program cylinder
     real(dp) :: height
     real(dp) :: circ_area
     real(dp) :: cyl_volume
+    real(dp) :: array(10)
+    real(dp) :: matrix(10, 10)
+    integer :: i
+
+    character(len=6) :: first_name
+    character(len=6) :: last_name
 
     pi = 3.1415927
     ! get radius
@@ -25,6 +31,12 @@ program cylinder
     ! print results
     print *, "The are of the base of you cylinder is: ", circ_area
     print *, "The volume of your cylinder is: ", cyl_volume
+
+
+     array = [(i, i = 1, 10)]
+     print *, array(1:10)
+     matrix = reshape(([(i, i = 1, 100)]), (/10,10/))
+     print *, matrix(1:10, 1:10)
 
 end program
 
